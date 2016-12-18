53d9040d7414f1ac1ac02802a5c3a048697915df
jsnelgro <johnny.snelgrove@gmail.com>
2016-12-18 21:19:11 +0100

    first commit

---
+++ spec/audio-recorder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AudioRecorderView from '../lib/audio-recorder-view';
+
+describe('AudioRecorderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
